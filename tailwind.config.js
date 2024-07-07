eb3551bdd13816449beeb4382921fb327240c790
abbas110 <meerabbas110@gmail.com>
2024-07-07 15:48:44 +0530

    tailwind intergration and project setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
+
+    "./app/**/*.{js,ts,jsx,tsx}", // Adjust according to your Next.js folder structure
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
