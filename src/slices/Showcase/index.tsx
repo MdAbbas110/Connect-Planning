2aa86a77bacfd088bbc9ba71948c953e085ba881
abbas110 <meerabbas110@gmail.com>
2024-07-24 01:42:29 +0530

    two more slices for the work show case

--- src/slices/Showcase/index.tsx
+++ src/slices/Showcase/index.tsx
@@ -3,6 +3,13 @@ import ButtonLink from "@/app/components/ButtonLink";
 import { Content } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import clsx from "clsx";
+import { PiArrowsClockwise, PiGear } from "react-icons/pi";
+
+const icons = {
+  gear: <PiGear />,
+  cycle: <PiArrowsClockwise />,
+};
 
 /**
  * Props for `Showcase`.
@@ -34,16 +41,32 @@ const Showcase = ({ slice }: ShowcaseProps): JSX.Element => {
 
       <div className="mt-16 grid items-center rounded-xl border border-blue-50/20 bg-gradient-to-b from-slate-50/15 to-slate-50/5 px-7 py-8 backdrop:blur-sm lg:grid-cols-3 lg:py-12">
         <div>
-          <>{slice.primary.icon}</>
-          <PrismicRichText field={slice.primary.subheading} />
-          <PrismicRichText field={slice.primary.body} />
+          <div className="w-fit rounded-lg bg-blue-500/35 p-4 text-3xl">
+            <>{slice.primary.icon && icons[slice.primary.icon]}</>
+          </div>
 
-          <ButtonLink field={slice.primary.button_link}>
-            {slice.primary.button_text}
+          <div className="mt-6 text-6xl font-normal">
+            <PrismicRichText field={slice.primary.subheading} />
+          </div>
+
+          <div className="prose prose-invert mt-4 max-w-xl">
+            <PrismicRichText field={slice.primary.body} />
+          </div>
+
+          <ButtonLink field={slice.primary.button_link} className="mt-6">
+            {slice.primary.button_text || "Learn more"}
           </ButtonLink>
         </div>
 
-        <PrismicNextImage field={slice.primary.image} />
+        <PrismicNextImage
+          field={slice.primary.image}
+          className={clsx(
+            "rounded-xl opacity-90 shadow-2xl lg:col-span-2 lg:pt-0",
+            slice.variation === "reverse"
+              ? "translate-x-[15%] lg:order-1"
+              : "translate-x-[-15%] lg:-order-1",
+          )}
+        />
       </div>
     </Bounded>
   );
